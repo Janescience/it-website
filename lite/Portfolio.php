246f27f467ee490e912234e9ed2f925641d1ca8b
Ch.gapps <ch.gapps@hotmail.com>
2018-02-06 08:50:49 +0700

    Show Portfolio

---
+++ lite/Portfolio.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head >
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+     <?php  include('header.php') ?>
+
+</head>
+
+<body class="fix-header fix-sidebar card-no-border logo-center">
+
+    <div class="preloader">
+      <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+
+    <div id="main-wrapper">
+
+    <?php  include('navbar.php') ?>
+
+      <div class="page-wrapper">
+        <div class="container-fluid">
+
+          <div class="row page-titles">
+
+          </div>
+
+
+<div class="row">
+
+
+<div class="card">
+  <div class="card-block">
+<div class="row el-element-overlay">
+                    <div class="col-md-12">
+                        <h2 class="card-title text-center"><i class="mdi mdi-school"></i> ระดับปริญญาตรี</h2>
+                        <h6 class="card-subtitle m-b-20 text-muted text-center">o o</h6> </div>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="card">
+                            <div class="el-card-item">
+                                <div class="el-card-avatar el-overlay-1"> <img id="imageHallofFameBachelor"  src="../images/BecPort.jpg" alt="user">
+                                    <div class="el-overlay">
+                                        <ul class="el-info">
+                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="BechelorHallofFame.php"><i class="icon-magnifier"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="el-card-content">
+                                    <h3 class="box-title">ผลงานดีเด่น</h3> <small>ผลงานดีเด่นของนักศึกษาระดับปริญญาตรี</small>
+                                    <br> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="card">
+                            <div class="el-card-item">
+                                <div class="el-card-avatar el-overlay-1"> <img id="imageGeneralBachelor" src="../images/GeneralPort2.jpg" alt="user">
+                                    <div class="el-overlay">
+                                        <ul class="el-info">
+                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="BechelorPortfolio.php"><i class="icon-magnifier"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="el-card-content">
+                                    <h3 class="box-title">ผลงานทั่วไป</h3> <small>ผลงานต่างๆของนักศึกษาระดับปริญญาตรี</small>
+                                    <br> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+
+<div class="card">
+  <div class="card-block">
+<div class="row el-element-overlay">
+                    <div class="col-md-12">
+                        <h2 class="card-title text-center"><i class="mdi mdi-school"></i> ระดับบัณฑิตศึกษา</h2>
+                        <h6 class="card-subtitle m-b-20 text-muted text-center">o o</h6> </div>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="card">
+                            <div class="el-card-item">
+                                <div class="el-card-avatar el-overlay-1"> <img id="imageHallofFameGraduate"  src="../images/GraPort.jpg" alt="user">
+                                    <div class="el-overlay">
+                                        <ul class="el-info">
+                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="GraduateHallofFame.php"><i class="icon-magnifier"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="el-card-content">
+                                    <h3 class="box-title">ผลงานดีเด่น</h3> <small>ผลงานดีเด่นของบัณฑิตศึกษา</small>
+                                    <br> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="card">
+                            <div class="el-card-item">
+                                <div class="el-card-avatar el-overlay-1"> <img id="imageGeneralBachelor" src="../images/GraPort2.jpg" alt="user">
+                                    <div class="el-overlay">
+                                        <ul class="el-info">
+                                            <li><a class="btn default btn-outline image-popup-vertical-fit" href="GraduatePortfolio.php"><i class="icon-magnifier"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="el-card-content">
+                                    <h3 class="box-title">ผลงานทั่วไป</h3> <small>ผลงานต่างๆของบัณฑิตศึกษา</small>
+                                    <br> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+            <footer class="footer">Copyright © Information Technology 2017</footer>
+        </div>
+    </div>
+
+    <?php include('import-javascript.php')?>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
